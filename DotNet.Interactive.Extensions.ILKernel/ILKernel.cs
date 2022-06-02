f3c408414065e03b40b25a4d12cb7f51fce601d6
Vlad Zarytovskii <vzaritovsky@hotmail.com>
2022-06-02 17:04:57 +0200

    Various refactorings

--- DotNet.Interactive.Extensions.ILKernel/ILKernel.cs
+++ DotNet.Interactive.Extensions.ILKernel/ILKernel.cs
@@ -1,58 +1,103 @@
 ﻿using System.Runtime.Loader;
 using Microsoft.DotNet.Interactive;
 using Microsoft.DotNet.Interactive.Commands;
+using Microsoft.DotNet.Interactive.Events;
+using Microsoft.DotNet.Interactive.Formatting;
 using Mobius.ILasm.Core;
-using Mobius.ILasm.interfaces;
+
+// namespace ILKernel.InteractiveExtension;
 
 namespace DotNet.Interactive.Extensions.ILKernel;
 
-public sealed class ILKernel : Kernel, IKernelCommandHandler<SubmitCode>
+#nullable disable
+
+public sealed class ILKernel :
+    Kernel,
+    ISupportNuget,
+    IKernelCommandHandler<SubmitCode>
 {
-    private static readonly ILogger _logger = new DummyILLogger();
+    private readonly Lazy<PackageRestoreContext> _packageRestoreContext = default;
 
-    public static void Activate() => ((CompositeKernel)Root).Add(new ILKernel());
+    private readonly List<string> _references = new();
 
-    public ILKernel() : base("il")
-    {
-    }
+    public PackageRestoreContext PackageRestoreContext => _packageRestoreContext.Value;
+
+    public IEnumerable<string> RestoreSources => PackageRestoreContext.RestoreSources;
+
+    public IEnumerable<PackageReference> RequestedPackageReferences => PackageRestoreContext.RequestedPackageReferences;
+
+    public IEnumerable<ResolvedPackageReference> ResolvedPackageReferences => PackageRestoreContext.ResolvedPackageReferences;
+    
+    public ILKernel() : base("il") =>
+        _packageRestoreContext = new(() =>
+        {
+            using PackageRestoreContext packageRestoreContext = new();
+            RegisterForDisposal(packageRestoreContext);
+
+            return packageRestoreContext;
+        });
 
     public Task HandleAsync(SubmitCode command, KernelInvocationContext context)
     {
+        using var logger = new PersistentILLogger();
         var cil = new[] { command.Code };
-        Driver driver = new(logger: _logger, target: Driver.Target.Exe, showParser: false, debuggingInfo: false, showTokens: false);
+        Driver driver = new(logger: logger, target: Driver.Target.Exe, showParser: false, debuggingInfo: false, showTokens: false);
 
-        using MemoryStream assmeblyStream = new();
+        using MemoryStream assemblyStream = new();
 
-        var succeeded = driver.Assemble(cil, assmeblyStream);
-
-        if (succeeded)
+        try
         {
-            assmeblyStream.Seek(0, SeekOrigin.Begin);
-
-            AssemblyLoadContext assemblyContext = new(null);
-            var assembly = assemblyContext.LoadFromStream(assmeblyStream);
-            var entryPoint = assembly.EntryPoint;
-
-            if (entryPoint is null)
-                throw new ApplicationException("Cannot find a valid entry point.");
-
-            var @params = entryPoint.GetParameters() switch
+            var succeeded = driver.Assemble(cil, assemblyStream);
+            if (succeeded)
             {
-                { Length: 0 } => null,
-                { Length: 1 } pi when pi.First().ParameterType == typeof(string[]) => new object[] { Array.Empty<string>() },
-                _ => throw new ApplicationException("Could not find a suitable entry point, should be either one of the following: void Main() or void Main(string[] args)")
-            };
+                assemblyStream.Seek(0, SeekOrigin.Begin);
 
-            var result = entryPoint?.Invoke(default, @params);
+                AssemblyLoadContext assemblyContext = new(null);
 
-            if (result is not null)
-                context.DisplayStandardOut(result?.ToString());
+                _references.ForEach(r => assemblyContext.LoadFromAssemblyPath(r));
+
+                var assembly = assemblyContext.LoadFromStream(assemblyStream);
+                var entryPoint = assembly.EntryPoint;
+
+                if (entryPoint is null)
+                    throw new ApplicationException("Cannot find a valid entry point.");
+
+                var @params = entryPoint.GetParameters() switch
+                {
+                    { Length: 0 } => null,
+                    { Length: 1 } pi when pi.First().ParameterType == typeof(string[]) => new object[] { Array.Empty<string>() },
+                    _ => throw new ApplicationException("Could not find a suitable entry point, should be either one of the following: void Main() or void Main(string[] args)")
+                };
+
+                var result = entryPoint?.Invoke(default, @params);
+
+                if (result is not null)
+                    context.DisplayStandardOut(result?.ToString());
+            }
+            else
+            {
+                // TODO: Get logs and diagnostics.
+                context.DisplayStandardError("Uh-oh. Something went wrong!");
+            }
         }
-        else
+        catch (Exception ex)
         {
-            // TODO: Get logs and diagnostics.
-            context.DisplayStandardError("Uh-oh. Something went wrong!");
+            var diagnostics = logger.GetDiagnostics().ToList();
+            var formattedDiagnostics = diagnostics.Select(d => new FormattedValue(PlainTextFormatter.MimeType, d.ToString())).ToArray();
+            context.Publish(new DiagnosticsProduced(diagnostics, command, formattedDiagnostics));
+            context.Fail(command, ex);
         }
+
         return Task.CompletedTask;
     }
+
+    public Task<PackageRestoreResult> RestoreAsync() => _packageRestoreContext.Value.RestoreAsync();
+
+    public PackageReference GetOrAddPackageReference(string packageName, string packageVersion = null) =>
+        _packageRestoreContext.Value.GetOrAddPackageReference(packageName, packageVersion);
+
+    public void TryAddRestoreSource(string source) => _packageRestoreContext.Value.TryAddRestoreSource(source);
+
+    public void RegisterResolvedPackageReferences(IReadOnlyList<ResolvedPackageReference> packageReferences) =>
+        _references.AddRange(packageReferences.SelectMany(r => r.AssemblyPaths));
 }
---
+++ DotNet.Interactive.Extensions.ILKernel/PersistentILLogger.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.DotNet.Interactive;
+using Mobius.ILasm.interfaces;
+
+using Diagnostic = Microsoft.DotNet.Interactive.Diagnostic;
+using Location = Mono.ILASM.Location;
+
+namespace DotNet.Interactive.Extensions.ILKernel;
+
+internal class PersistentILLogger : ILogger, IDisposable
+{
+    private enum Severity { Warning, Error };
+
+    private readonly List<(Severity, Location, string)> _storage = new();
+
+    internal IEnumerable<Diagnostic> GetDiagnostics() => _storage.Select(
+        d =>
+        {
+            var (severity, location, message) = d;
+            // Position is adjusted for the fact that the ILASM source is 1-based,
+            LinePosition linePos = new(location.line - 1, location.column - 1);
+            LinePositionSpan span = new(linePos, linePos);
+
+            var diagnosticSeverity = severity is Severity.Warning
+                                   ? DiagnosticSeverity.Warning
+                                   : DiagnosticSeverity.Error;
+
+            return new Diagnostic(span, diagnosticSeverity, "0", message);
+        });
+
+    public void Dispose() => _storage.Clear();
+
+    public void Error(string message)
+    {
+    }
+
+    public void Error(Location location, string message) => _storage.Add((Severity.Error, location, message));
+
+    public void Info(string message)
+    {
+    }
+
+    public void Warning(string message)
+    {
+    }
+
+    public void Warning(Location location, string message) => _storage.Add((Severity.Warning, location, message));
+}
